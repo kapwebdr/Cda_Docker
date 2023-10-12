f4ea248c95f044be0a20a1de7db57e027f40ac92
kapwebdr <damien@kapweb.com>
2023-10-12 10:21:22 +0200

    Sorry guys

--- Projects/Model/Db.php
+++ Projects/Model/Db.php
@@ -6,9 +6,7 @@ class Db
 {
     static $db=null;
     private array $datas;
-    private array $where= [];
-    private array $order=[];
-
+    
     static function Connect()
     {
         if(is_null(self::$db))
@@ -23,7 +21,6 @@ class Db
             }
         }
     }
-    
     public function __construct()
     {
         self::Connect();
@@ -38,22 +35,11 @@ class Db
         return $this->datas[$var];
     }
 
-    public function Where(array $where=[]):object
-    {
-        $this->Where = $where;
-        return $this;
-    }
-
-    public function Order(array $order=[]):object
-    {
-        $this->Ohere = $order;
-        return $this;
-    }
     public function Save()
     {
         $columns = array_keys($this->datas);
 
-        $sql    = 'INSERT into '.$this->table.' set ';
+        $sql    = 'insert into '.$this->table.' set ';
 
         foreach($columns as $key=>$column)
         {
@@ -66,78 +52,31 @@ class Db
         $rq->execute($this->datas);
         return self::$db->lastInsertId();
     }
-    public function Update()
+
+    static function Update()
     {
-        $columns = array_keys($this->datas);
 
-        $sql    = 'UPDATE '.$this->table.' set ';
-
-        foreach($columns as $key=>$column)
-        {
-            $sql   .= $column.'=:'.$column;
-            if($key < (count($columns)-1))
-            $sql   .= ',';
-        }
-
-        $rq = self::$db->prepare($sql);
-        return $rq->execute($this->datas);
     }
 
-    public function Delete()
+    static function Delete()
     {
-        $columns = array_keys($this->where);
 
-        $sql    = 'DELETE FROM '.$this->table.' WHERE ';
-
-        foreach($columns as $key=>$column)
-        {
-            $sql   .= $column.'=:'.$column;
-            if($key < (count($columns)-1))
-            $sql   .= ' AND ';
-        }
-
-        $rq = self::$db->prepare($sql);
-        return $rq->execute($this->datas);
     }
 
-    public function Find()
+    static function Find($sql,$datas)
     {
-        $columns = array_keys($this->where);
-
-        $sql    = 'SELECT * FROM '.$this->table.' WHERE ';
-
-        foreach($columns as $key=>$column)
-        {
-            $sql   .= $column.'=:'.$column;
-            if($key < (count($columns)-1))
-            $sql   .= ',';
-        }
-
         $rq = self::$db->prepare($sql);
-        $rq->execute($this->datas);
-        
+        $rq->execute($datas);
         return $rq->fetchAll();
     }
 
-    public function FindOne()
+    static function FindOne($sql,$datas)
     {
-        $columns = array_keys($this->where);
-
-        $sql    = 'SELECT * FROM '.$this->table.' WHERE ';
-
-        foreach($columns as $key=>$column)
-        {
-            $sql   .= $column.'=:'.$column;
-            if($key < (count($columns)-1))
-            $sql   .= ',';
-        }
-
-        $sql    .= ' LIMIT 1';
-        $rq      = self::$db->prepare($sql);
-        $rq->execute($this->datas);
-        
+        $rq = self::$db->prepare($sql);
+        $rq->execute($datas);
         return $rq->fetch();
     }
+
 }
 
 ?>
